f0a304684bd2ed31c94ab688d0fa0e890b6d9247
Blake <lakebayomackway@gmail.com>
2017-04-10 10:27:51 -0700

    add Book.java, BookTest.java, instantiation test, instantiates with name test.

---
+++ src/main/java/Book.java
@@ -0,0 +1,22 @@
+import org.sql2o.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.sql.Timestamp;
+
+public class Book {
+  private String name;
+  private int id;
+
+  public Book(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+}//end of file
