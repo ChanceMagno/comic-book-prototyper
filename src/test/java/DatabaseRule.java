01913df29cfb2cac838d49857bf9a5833ba95332
Blake <lakebayomackway@gmail.com>
2017-04-10 13:00:09 -0700

    add third test to Book, get iD.

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,12 +5,14 @@ public class DatabaseRule extends ExternalResource {
 
   @Override
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/comic_book_prototyper_test", null, null);
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/comics_test", null, null);
   }
 
   @Override
   protected void after() {
     try(Connection con = DB.sql2o.open()) {
+      String deleteBookQuery = "DELETE FROM books *;";
+      con.createQuery(deleteBookQuery).executeUpdate();
       String deletePanelQuery = "DELETE FROM panels *;";
       con.createQuery(deletePanelQuery).executeUpdate();
       String deletePageQuery = "DELETE FROM pages *;";
