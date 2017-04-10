00c707db91e5d5f596f43d5d9282fd787e4075cb
Blake <lakebayomackway@gmail.com>
2017-04-10 13:57:06 -0700

    add test suite to BookTest, methods to Book, commented out.

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -8,15 +8,15 @@ import java.util.TimerTask;
 import java.sql.Timestamp;
 
 public class Book {
-  private String name;
+  private String title;
   private int id;
 
-  public Book(String name) {
-    this.name = name;
+  public Book(String title) {
+    this.title = title;
   }
 
-  public String getName() {
-    return name;
+  public String getTitle() {
+    return title;
   }
 
   public int getId() {
@@ -25,12 +25,82 @@ public class Book {
 
   public void save() {
     try(Connection con = DB.sql2o.open()) {
-      String sql = "INSERT INTO books(name) VALUES(:name)";
+      String sql = "INSERT INTO books(title) VALUES(:title)";
       this.id = (int) con.createQuery(sql, true)
-        .addParameter("name", this.name)
+        .addParameter("title", this.title)
         .executeUpdate()
         .getKey();
     }
   }
+//new below
+
+// public static List<Book> all() {
+//   String sql = "SELECT id, title FROM books";
+//   try(Connection con = DB.sql2o.open()) {
+//     return con.createQuery(sql).executeAndFetch(Book.class);
+//   }
+// }
+//
+// public static Book find(int id) {
+//      try(Connection con = DB.sql2o.open()) {
+//        String sql = "SELECT * FROM books where id=:id";
+//        Book book = con.createQuery(sql)
+//          .addParameter("id", id)
+//          .executeAndFetchFirst(Book.class);
+//        return book;
+//      }
+//    }
+//
+//    public List<Client> getClients() {
+//    try(Connection con = DB.sql2o.open()) {
+//      String sql = "SELECT * FROM clients where bookId=:id";
+//      return con.createQuery(sql)
+//        .addParameter("id", this.id)
+//        .executeAndFetch(Client.class);
+//    }
+//  }
+//
+//    @Override
+//    public boolean equals(Object otherBook) {
+//      if (!(otherBook instanceof Book)) {
+//        return false;
+//      } else {
+//        Book newBook = (Book) otherBook;
+//        return this.getId() == newBook.getId() &&
+//               this.getName().equals(newBook.getName());
+//      }
+//  }
+//
+//   public void save() {
+//     try(Connection con = DB.sql2o.open()) {
+//       String sql = "INSERT INTO books(title) VALUES (:title)";
+//       this.id = (int) con.createQuery(sql, true)
+//         .addParameter("title", this.title)
+//         .executeUpdate()
+//         .getKey();
+//     }
+//   }
+//
+//   public void update(String title) {
+//   try(Connection con = DB.sql2o.open()) {
+//     String sql = "UPDATE books SET title = :title WHERE id = :id;";
+//     con.createQuery(sql)
+//       .addParameter("title", title)
+//       .addParameter("id", id)
+//       .executeUpdate();
+//     }
+//   }
+//
+//   public void delete() {
+//    try(Connection con = DB.sql2o.open()) {
+//      String sql = "DELETE FROM books WHERE id = :id;";
+//      con.createQuery(sql)
+//        .addParameter("id", id)
+//        .executeUpdate();
+//      }
+//    }
+
+
+
 
 }//end of file
--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -21,18 +21,73 @@ public class BookTest {
   }
 
   @Test
-  public void getBookName_returnsBookName_string() {
+  public void getBookTitle_returnsBookTitle_string() {
     Book myBook = new Book("Comic One");
-    assertEquals("Comic One", myBook.getName());
+    assertEquals("Comic One", myBook.getTitle());
   }
 
   @Test
-   public void getId_bookInstantiateWithAnId_1() {
+   public void getId_bookInstantiatesWithAnId_1() {
      Book myBook = new Book("Comic One");
      myBook.save();
      assertTrue(myBook.getId() > 0);
    }
 
+   //new below
 
+ //   @Test
+ //  public void equals_returnsTrueIfNameIsTheSame_true() {
+ //    Book firstBook = new Book("Comic One");
+ //    Book anotherBook = new Book("Comic Two");
+ //    assertTrue(firstBook.equals(anotherBook));
+ //  }
+ //
+ //  @Test
+ // public void save_assignsIdToObjectAndSavesObjectToDatabase() {
+ //   Book testBook = new Book("Comic One");
+ //   testBook.save();
+ //   Book savedBook = Book.all().get(0);
+ //   assertEquals(testBook.getId(), savedBook.getId());
+ // }
+ //
+ // @Test
+ // public void all_returnsAllInstancesOfBook_true() {
+ //   Book firstBook = new Book("Comic One");
+ //   firstBook.save();
+ //   Book secondBook = new Book("Comic Two");
+ //   secondBook.save();
+ //   assertEquals(true, Book.all().get(0).equals(firstBook));
+ //   assertEquals(true, Book.all().get(1).equals(secondBook));
+ // }
+ //
+ // @Test
+ // public void find_returnsBookWithSameId_secondBook() {
+ //   Book firstBook = new Book("Comic One");
+ //   firstBook.save();
+ //   Book secondBook = new Book("Comic Two");
+ //   secondBook.save();
+ //   assertEquals(Book.find(secondBook.getId()), secondBook);
+ // }
+ //
+ // @Test
+ // public void updateName_updatesBookNameInDatabase_String() {
+ //   Book testBook = new Book("Comic One");
+ //   testBook.save();
+ //   testBook.updateName("Buck");
+ //   assertEquals("Buck", Book.find(testBook.getId()).getName());
+ // }
+ //
+ // @Test
+ // public void delete_deletesBookFromDatabase_0() {
+ //   Book testBook = new Book("Comic One");
+ //   testBook.save();
+ //   testBook.delete();
+ //   assertEquals(0, Book.all().size());
+ // }
+ //
+ // @Test
+ // public void find_returnsNullWhenNoBookFound_null() {
+ //   assertTrue(Book.find(999) == null);
+ // }
 
  }
