7e5b84fe10e79f149e87a30b886beb55bf1032a1
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-04-11 10:08:23 -0700

    Add remaining Text and ComicPanel methods and tests

--- src/main/java/Text.java
+++ src/main/java/Text.java
@@ -25,16 +25,32 @@ public class Text {
     return panel_id;
   }
 
+  public void setPanelId(int panel_id) {
+    this.panel_id = panel_id;
+  }
+
   public int getSequence() {
     return sequence;
+  }
+
+  public void setSequence(int sequence) {
+    this.sequence = sequence;
   }
 
   public String getBoxStyle() {
     return box_style;
   }
 
+  public void setBoxStyle(String box_style) {
+    this.box_style = box_style;
+  }
+
   public String getFont() {
     return font;
+  }
+
+  public void setFont(String font) {
+    this.font = font;
   }
 
   public void save() {
@@ -55,6 +71,50 @@ public class Text {
       String sql = "SELECT * FROM texts;";
       return con.createQuery(sql)
         .executeAndFetch(Text.class);
+    }
+  }
+
+  public static Text find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM texts WHERE id = :id;";
+      return con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Text.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherText) {
+    if(!(otherText instanceof Text)) {
+      return false;
+    } else {
+      Text newText = (Text) otherText;
+      return this.getPanelId() == newText.getPanelId() &&
+      this.getSequence() == newText.getSequence() &&
+      this.getBoxStyle().equals(newText.getBoxStyle()) &&
+      this.getFont().equals(newText.getFont());
+    }
+  }
+
+  public void update() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE texts SET panel_id = :panel_id, sequence = :sequence, box_style = :box_style, font = :font WHERE id = :id;";
+      con.createQuery(sql)
+        .addParameter("panel_id", this.panel_id)
+        .addParameter("sequence", this.sequence)
+        .addParameter("box_style", this.box_style)
+        .addParameter("font", this.font)
+        .addParameter("id", this.id)
+        .executeUpdate();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM texts WHERE id = :id;";
+      con.createQuery(sql)
+        .addParameter("id", id)
+        .executeUpdate();
     }
   }
 }
