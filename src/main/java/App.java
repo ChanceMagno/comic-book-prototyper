7feea15e625427de78b91b9a0254f4591d298b32
chance <chancemagno@gmail.com>
2017-04-11 15:49:11 -0700

    add to App.java /book/:id/layout when submitted creates panel based on layout chosen

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,32 +18,41 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/books/new", (request, response) -> {
+    get("/book/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/comic-panel-form.vtl");
+      model.put("template", "templates/book-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/books/new", (request, response) -> {
+    post("/book/page", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      String book = request.queryParams("new-book");
-      Book newBook = new Book("new book", 1);
-      model.put("template", "templates/comic-panel-form.vtl");
+      String bookTitle = request.queryParams("title");
+      Book book = new Book(bookTitle, 1);
+      book.save();
+      model.put("book", book);
+      model.put("template", "templates/pages.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-
-    get("/pages/new", (request, response) -> {
+    post("/book/:id/layout", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/comic-panel-form.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    post("/books/new", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      String book = request.queryParams("new-book");
-      Book newBook = new Book("new book", 1);
-      model.put("template", "templates/.vtl");
+      Book book = Book.find(Integer.parseInt(request.params(":id")));
+      String layoutPicked = request.queryParams("layout");
+      Page page = new Page(book.getId(), layoutPicked);
+      page.save();
+        int panelsNeeded = 0;
+        if (layoutPicked.equals("layout1")) {
+             panelsNeeded = 6;
+        }
+          Integer panelSequence = 0;
+        for(int i = 0; i < panelsNeeded; i++ ) {
+          panelSequence += i;
+          ComicPanel panel = new ComicPanel(page.getId(), i);
+          panel.save();
+        }
+      model.put("book", book);
+      model.put("page", page);
+      model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
